e0f9e17ff354b1c048dde226d6ffe4ec0827bc96
Momoy <irosyadi16@gmail.com>
2021-04-19 02:52:28 +0700

    Initial commit

---
+++ app/src/main/java/com/lomboktech/mangaapp/api/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.lomboktech.mangaapp.api;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://mangamint.kaedenoki.net/api/manga/page/";
+    public static String ALSOURL = "https://mangamint.kaedenoki.net/api/recommended";
+    public static String GENREURL = "https://mangamint.kaedenoki.net/api/genres";
+    public static String DETAILMANGAURL = "https://mangamint.kaedenoki.net/api/manga/detail/{endpoint}";
+    public static String CHAPTERURL = "https://mangamint.kaedenoki.net/api/chapter/{chapter_endpoint}";
+    public static String GENREDETAIL = "https://mangamint.kaedenoki.net/api/genres/{endpoint}";
+
+}
